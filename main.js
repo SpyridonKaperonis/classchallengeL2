c87183ec38dcd4c738fa0bb616a49952446c360a
Spyridon Kaperonis <42813038+SpyridonKaperonis@users.noreply.github.com>
2020-03-02 10:44:17 -0600

    some css

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-const  calcArea = (students, positions) => {
+const  calcstudents = (students, positions) => {
     return students / positions;
   }
+  
+
+  document.querySelector('#submit').addEventListener('click',  () => {
+  const i = parseInt(document.getElementById('#Students').value)
+  const j = parseInt(document.getElementById('#Position').value)
+  const ans = `${s}, Students per position is ${calcstudents(i, j)}.`
+  document.getElementById('#display').innerHTML = calcstudents(i,j)
+  });
+
+ 
