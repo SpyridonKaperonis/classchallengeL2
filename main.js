becc1d14c3d8d6f875df3b50cde9f420134f3b1c
Spyridon Kaperonis <42813038+SpyridonKaperonis@users.noreply.github.com>
2020-03-02 10:47:12 -0600

    changes

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const  calcstudents = (students, positions) => {
   
 
   document.querySelector('#submit').addEventListener('click',  () => {
-  const i = parseInt(document.getElementById('#Students').value)
-  const j = parseInt(document.getElementById('#Position').value)
-  const ans = `${s}, Students per position is ${calcstudents(i, j)}.`
+  var i = parseInt(document.getElementById('#Students').value)
+  var j = parseInt(document.getElementById('#Position').value)
+  var ans = `${s}, Students per position is ${calcstudents(i, j)}.`
   document.getElementById('#display').innerHTML = calcstudents(i,j)
   });
 
